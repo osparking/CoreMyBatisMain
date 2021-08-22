467422a5a244369b5f4ac5a97a8dec43c195eb01
Park,JongBum <jbpark03@gmail.com>
2021-08-22 13:23:26 +0900

    2.3절 사례1 완료. DB table user, pet dumped
    withSpring Maven spring version 3.0.7

--- src/com/ezen/mybatis/core/PetDaoImpl.java
+++ src/com/ezen/mybatis/core/PetDaoImpl.java
@@ -1,5 +1,6 @@
 package com.ezen.mybatis.core;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -17,5 +18,12 @@ public class PetDaoImpl implements PetDAO {
 	public void setSqlSessionTemplate(SqlSessionTemplate
 			sqlSessionTemplate) { 
 		this.sqlSessionTemplate = sqlSessionTemplate; 
+	}
+
+	@Override
+	public PetDVO getPetObject(String petName) throws Exception {
+		HashMap<String, String> inputMap = new HashMap<String, String>();
+		inputMap.put("name", petName);
+		return (PetDVO) sqlSessionTemplate.selectOne("getPetObject", inputMap);
 	}
 }
